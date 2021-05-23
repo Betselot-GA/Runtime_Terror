ea8c5c29b51a0785b409329cec7426ff92f99426
geogetnet <geogetnet@yahoo.com>
2021-05-23 13:59:35 +0300

    subtraction edited-2

--- subtraction.py
+++ subtraction.py
@@ -11,42 +11,42 @@ size1 = len(num1)
 size2 = len(num2)
 
 if(size1 == size2 and size1 > 1):
-    if num1 == num2:
-        print(0)
-    else:
-        if num1 < num2:
-            temp = num1
-            num1 = num2
-            num2 = temp
-            flag = 1
-        for i in num1:
-            n1.append(i)
-        for j in num2:
-            n2.append(j)
+    if num1 < num2:
+        temp = num1
+        num1 = num2
+        num2 = temp
+        flag = 1
+    for i in num1:
+        n1.append(i)
+    for j in num2:
+        n2.append(j)
         
             
-        for i in range(1,size1+1):
-            diff = 0
-            if len(result) > 0:
-                if (int(n1[size1-i]) < int(n2[size2-i])):
-                    n1[size1 - i] = "1" + str(n1[size1 - i])
-                    n1[size1 - i] =  int(n1[size1 - i])-1
-                    carry = 1
-                else:
-                    diff = int(n1[size1 - i]) - int(n2[size1 - i])
+    for i in range(1,size1+1):
+        diff = 0
+        if len(result) > 0:
+            if (int(n1[size1-i]) < int(n2[size2-i])):
+                n1[size1 - i] = "1" + str(n1[size1 - i])
+                diff = int(n1[size1 - i]) - int(n2[size1 - i])
+                carry = 1
+            else:
+                diff = int(n1[size1 - i]) - int(n2[size1 - i])
             if carry == 1:
                 n1[size1 - i] =  int(n1[size2 - i])-1
-            if(len(result)==0) and int(n1[size1-i]) < int(n2[size2-i]):
-                    n1[size1 - i] = "1" + str(n1[size1 - i])
-            elif((len(result)==0) and int(n1[size1-i]) > int(n2[size2-i])): 
-                diff = int(n1[size1 - i]) - int(n2[size1 - i])
+        if(len(result)==0) and int(n1[size1-i]) < int(n2[size2-i]):
+            print("debug 1")
+            n1[size1 - i] = "1" + str(n1[size1 - i])
+            diff = int(n1[size1 - i]) - int(n2[size1 - i])
+            carry = 1
+        elif((len(result)==0) and int(n1[size1-i]) > int(n2[size2-i])): 
+            diff = int(n1[size1 - i]) - int(n2[size1 - i])
+            
                 
-            result.append(diff)
-        if flag == 1:
-            print("-",end="")
-        for i in range(1,len(result)+1):
-                
-            print(result[len(result)-i],end="")
+        result.append(diff)
+    if flag == 1:
+        print("-",end="")
+    for i in range(1,len(result)+1):    
+        print(result[len(result)-i],end="")
     
     
         
